149a6f43f8efebd7c6fdc1caa1a0249966b402f7
lcngh <140435105+lcngh@users.noreply.github.com>
2023-08-10 09:05:30 -0400

    geometry.h vec3f class update

--- geometry.h
+++ geometry.h
@@ -8,7 +8,18 @@ vec3 class based off of peter shirley's implementation in Ray Tracing Weekend Bo
 */
 
 class Vec3f {
+    public:
+        // constructor(parameters) : initialization list {constructor body}
+        Vec3f() : e{0,0,0} {}
+        Vec3f(double x, double y, double z) : e{x,y,z} {}
+        Vec3f(double x, double y) : e{x,y,0.0} {}
 
+
+
+    public:
+
+        // array for {x,y,z} etc ...
+        double e[3];    
 };
 
 #endif
